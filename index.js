78da9883d6a06f65bc47d87d0d2d02c2d90e3dc5
t2warrior <iromadhon15@gmail.com>
2021-10-20 14:47:46 +0700

    mainline

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js')
+const DisTube = require('distube')
+const { prefix, token } = require('./config.json')
+const client = new Discord.Client({
+	intents: [
+		'GUILDS',
+		'GUILD_VOICE_STATES',
+		'GUILD_MESSAGES',
+	],
+})
+
+client.once('ready', () => {
+	console.log('Logged in!')
+})
+
+client.login(token)
