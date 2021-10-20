f1b5d79e00aa3366f99a2aab297b86a941733f03
t2warrior <iromadhon15@gmail.com>
2021-10-20 15:27:55 +0700

    Add Commands 'Ping'

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const Discord = require('discord.js')
-const DisTube = require('distube')
-const { prefix, token } = require('./config.json')
+//Requirements
+const fs = require('fs');
+const Discord = require('discord.js');
+const DisTube = require('distube');
+const config = require('./config.json');
+const prefix = config.prefix;
+const token = config.token;
 const client = new Discord.Client({
 	intents: [
 		'GUILDS',
@@ -9,8 +13,34 @@ const client = new Discord.Client({
 	],
 })
 
+//const distube = new DisTube(client, { searchSongs: true, emitNewSongOnly: true });
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles){
+	const command = require(`./commands/${file}`)
+	client.commands.set(command.name, command)
+}
+
 client.once('ready', () => {
-	console.log('Logged in!')
+	console.log('Ready at Your Service!')
 })
+client.on('ready', function() {
+	client.user.setActivity(config.activity, { type: config.activityType });
+  });
+
+//Use Your Command Here
+client.on('message', async(message) => {
+	if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if(command == 'ping'){
+		client.commands.get('ping').execute(message, args)
+	}
+}) 
 
 client.login(token)
